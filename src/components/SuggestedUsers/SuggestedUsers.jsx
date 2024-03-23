229408c71b686d2a7cf2e72e58c1b147109c147a
Yash <yanbhore@gmail.com>
2024-03-24 00:35:56 +0530

    Search Functionality completed

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,24 +1,31 @@
 import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
 import SuggestedUser from "./SuggestedUser";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
 const SuggestedUsers = () => {
+	const { isLoading, suggestedUsers } = useGetSuggestedUsers();
+
+	if (isLoading) return null;
+
 	return (
 		<VStack py={8} px={6} gap={4}>
 			<SuggestedHeader />
 
-			<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-				<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-					Suggested for you
-				</Text>
-				<Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
-					See All
-				</Text>
-			</Flex>
+			{suggestedUsers.length !== 0 && (
+				<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+					<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+						Suggested for you
+					</Text>
+					<Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+						See All
+					</Text>
+				</Flex>
+			)}
 
-			<SuggestedUser name='Baburao' followers={1392} avatar='S1.png' />
-			<SuggestedUser name='Ganpatrao' followers={567} avatar='S1.png' />
-			<SuggestedUser name='Apte' followers={759} avatar='S1.png' />
+			{suggestedUsers.map((user) => (
+				<SuggestedUser user={user} key={user.id} />
+			))}
 
 			
 		</VStack>
